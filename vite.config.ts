0c102269a689b6b50c73395e09836a76b3496be7
Azat <azatidiatov137@gmail.com>
2024-07-14 14:16:37 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
+import path, { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import VueDevTools from "vite-plugin-vue-devtools";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+
+  plugins: [
+    vue(),
+    eslintPlugin(),
+    tsconfigPaths(),
+    VueDevTools({ componentInspector: false }),
+  ],
+
+  server: {
+    port: 8080,
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+    extensions: [".ts", ".js", ".vue"],
+  },
+
+  build: {
+    outDir: path.join(__dirname, "dist"),
+    rollupOptions: {
+      input: resolve(__dirname, "index.html"),
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+          '@import "src/scss/variables.scss"; @import "src/scss/mixins.scss";',
+      },
+    },
+  },
+});
